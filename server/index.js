1d795affb7e4d62802c207c8f54f42a5de9b9ca6
Rafa Moral <rafael.moral@adevinta.com>
2022-09-13 12:33:42 +0200

    fix(Root): fix server error

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 var http = require('http'),
     fs = require('fs');
 
-
-const html = '<!DOCTYPE html><html><head><title>Page Title</title></head><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>';
+var html = '<!DOCTYPE html><html><head><title>Page Title</title></head><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>';
 
 http.createServer(function(request, response) {  
     response.writeHeader(200, {"Content-Type": "text/html"});
