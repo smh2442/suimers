e4223a06b44c8b398d82ff84feb241eb6ba29299
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2022-12-20 12:41:26 +0100

    feat(Root): publish package

--- src/generateCommentFromResults.js
+++
@@ -1,9 +0,0 @@
-const mapLhciResults = require('./mapLhciResults.js')
-const fromResultsToCommentMapper = require('./fromResultsToCommentMapper.js')
-
-module.exports = ({results = []}) => {
-  const formattedResults = mapLhciResults(results)
-  const comment = fromResultsToCommentMapper(formattedResults)
-
-  return comment
-}
